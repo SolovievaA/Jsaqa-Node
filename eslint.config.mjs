4394c452589c8cadb5082be4ae1219dd96e7c771
Alla QA <allushka181@gmail.com>
2024-09-19 17:43:28 +0300

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    {
+        rules: {
+            "no-var": "error",
+            semi: "error",
+            indent: "error",
+            "no-multi-spaces": "error",
+            "no-regex-spaces": "error",
+            "space-in-parens": "error",
+            "no-multiple-empty-lines": "error",
+            "prefer-const": "error",
+            "no-use-before-define": "error",
+            "no-extra-semi": "error",
+        },
+    },
+];
